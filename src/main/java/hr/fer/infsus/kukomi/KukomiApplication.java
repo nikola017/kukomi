cc28fab8712f8afcaeb224495d6dcac5afbfebd3
Nikola Bukvic <117097070+nikola017@users.noreply.github.com>
2024-05-23 18:48:30 +0200

    Initial commit

---
+++ src/main/java/hr/fer/infsus/kukomi/KukomiApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.infsus.kukomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukomiApplication.class, args);
+	}
+
+}
